84c2d4269edef4daf5e3258d441e8d211eb257a9
Stuart Gunter <stuart.gunter@gmail.com>
2014-05-08 21:24:52 +0100

    Annotated config properties

--- src/main/java/uk.co.composable.dropwizard.cassandra/CassandraConfiguration.java
+++ src/main/java/uk.co.composable.dropwizard.cassandra/CassandraConfiguration.java
@@ -3,6 +3,7 @@ package uk.co.composable.dropwizard.cassandra;
 import com.datastax.driver.core.AuthProvider;
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.ProtocolOptions;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Strings;
 import org.hibernate.validator.constraints.NotEmpty;
 
@@ -34,26 +35,102 @@ public class CassandraConfiguration {
 
     private int shutdownWaitSeconds = 20;
 
+    @JsonProperty
     public String getClusterName() {
         return clusterName;
     }
 
+    @JsonProperty
     public void setClusterName(String clusterName) {
         this.clusterName = clusterName;
     }
 
+    @JsonProperty
     public String getKeyspace() {
         return keyspace;
     }
 
+    @JsonProperty
     public void setKeyspace(String keyspace) {
         this.keyspace = keyspace;
     }
 
+    @JsonProperty
+    public String[] getContactPoints() {
+        return contactPoints;
+    }
+
+    @JsonProperty
+    public void setContactPoints(String[] contactPoints) {
+        this.contactPoints = contactPoints;
+    }
+
+    @JsonProperty
+    public int getPort() {
+        return port;
+    }
+
+    @JsonProperty
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    @JsonProperty
+    public int getProtocolVersion() {
+        return protocolVersion;
+    }
+
+    @JsonProperty
+    public void setProtocolVersion(int protocolVersion) {
+        this.protocolVersion = protocolVersion;
+    }
+
+    @JsonProperty
+    public AuthProvider getAuthProvider() {
+        return authProvider;
+    }
+
+    @JsonProperty
+    public void setAuthProvider(AuthProvider authProvider) {
+        this.authProvider = authProvider;
+    }
+
+    @JsonProperty
+    public ProtocolOptions.Compression getCompression() {
+        return compression;
+    }
+
+    @JsonProperty
+    public void setCompression(ProtocolOptions.Compression compression) {
+        this.compression = compression;
+    }
+
+    @JsonProperty
+    public boolean isMetricsEnabled() {
+        return metricsEnabled;
+    }
+
+    @JsonProperty
+    public void setMetricsEnabled(boolean metricsEnabled) {
+        this.metricsEnabled = metricsEnabled;
+    }
+
+    @JsonProperty
+    public boolean isJmxEnabled() {
+        return jmxEnabled;
+    }
+
+    @JsonProperty
+    public void setJmxEnabled(boolean jmxEnabled) {
+        this.jmxEnabled = jmxEnabled;
+    }
+
+    @JsonProperty
     public int getShutdownWaitSeconds() {
         return shutdownWaitSeconds;
     }
 
+    @JsonProperty
     public void setShutdownWaitSeconds(int shutdownWaitSeconds) {
         this.shutdownWaitSeconds = shutdownWaitSeconds;
     }
