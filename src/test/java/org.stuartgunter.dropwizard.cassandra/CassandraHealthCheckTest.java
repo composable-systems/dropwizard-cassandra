b4ddd8a12e6f2d0c15acb6fd7ee70d54f06820b4
Stuart Gunter <stuart.gunter@gmail.com>
2014-05-09 22:00:35 +0100

    Added CassandraHealthCheck tests

--- src/test/java/org.stuartgunter.dropwizard.cassandra/CassandraHealthCheckTest.java
+++ src/test/java/org.stuartgunter.dropwizard.cassandra/CassandraHealthCheckTest.java
@@ -1,21 +1,60 @@
 package org.stuartgunter.dropwizard.cassandra;
 
+import com.codahale.metrics.health.HealthCheck;
+import com.datastax.driver.core.Cluster;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.*;
+
 public class CassandraHealthCheckTest {
+
+    private final Cluster cluster = mock(Cluster.class);
+
     @Test
     public void isHealthyIfNoExceptionIsThrownOnConnectionToCluster() throws Exception {
+        final CassandraHealthCheck healthCheck = new CassandraHealthCheck(cluster);
+
+        final HealthCheck.Result result = healthCheck.execute();
+
+        assertThat(result.isHealthy(), is(true));
+        verify(cluster).connect();
+        verifyNoMoreInteractions(cluster);
     }
 
     @Test
     public void isUnhealthyIfAnExceptionIsThrownOnFailedConnectionToCluster() throws Exception {
+        when(cluster.connect()).thenThrow(new RuntimeException());
+        final CassandraHealthCheck healthCheck = new CassandraHealthCheck(cluster);
+
+        final HealthCheck.Result result = healthCheck.execute();
+
+        assertThat(result.isHealthy(), is(false));
+        verify(cluster).connect();
+        verifyNoMoreInteractions(cluster);
     }
 
     @Test
     public void isHealthyIfNoExceptionIsThrownOnConnectionToKeyspace() throws Exception {
+        final CassandraHealthCheck healthCheck = new CassandraHealthCheck(cluster, "keyspace");
+
+        final HealthCheck.Result result = healthCheck.execute();
+
+        assertThat(result.isHealthy(), is(true));
+        verify(cluster).connect("keyspace");
+        verifyNoMoreInteractions(cluster);
     }
 
     @Test
     public void isUnhealthyIfAnExceptionIsThrownOnFailedConnectionToKeyspace() throws Exception {
+        when(cluster.connect(anyString())).thenThrow(new RuntimeException());
+        final CassandraHealthCheck healthCheck = new CassandraHealthCheck(cluster, "keyspace");
+
+        final HealthCheck.Result result = healthCheck.execute();
+
+        assertThat(result.isHealthy(), is(false));
+        verify(cluster).connect("keyspace");
+        verifyNoMoreInteractions(cluster);
     }
 }
