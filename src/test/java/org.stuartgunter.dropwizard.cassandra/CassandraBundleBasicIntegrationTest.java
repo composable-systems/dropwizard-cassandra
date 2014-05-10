8cbc67853869eab6f15a10819779c48188ff05cc
Stuart Gunter <stuart.gunter@gmail.com>
2014-05-10 16:52:14 +0100

    Added integration test to prove full configuration

---
+++ src/test/java/org.stuartgunter.dropwizard.cassandra/CassandraBundleBasicIntegrationTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2014 Stuart Gunter
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.stuartgunter.dropwizard.cassandra;
+
+import com.google.common.collect.Lists;
+import com.google.common.io.Resources;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class CassandraBundleBasicIntegrationTest {
+
+    @ClassRule
+    public static final DropwizardAppRule<CassandraBundleConfiguration> APP =
+            new DropwizardAppRule<>(CassandraBundleApp.class, Resources.getResource("cassandra-basic.yml").getPath());
+
+    @Test
+    public void canQueryCassandra() throws Exception {
+        final URI uri = UriBuilder.fromUri("http://localhost")
+                .port(APP.getLocalPort())
+                .path("query")
+                .build();
+
+        final WebResource resource = Client.create().resource(uri);
+        final List<String> result = Lists.newArrayList(resource.get(String[].class));
+
+        assertThat(result, hasItem("system"));
+    }
+
+    @Test
+    public void cassandraMetricsArePublished() throws Exception {
+        final URI uri = UriBuilder.fromUri("http://localhost")
+                .port(APP.getLocalPort() + 1)
+                .path("metrics")
+                .build();
+
+        final WebResource resource = Client.create().resource(uri);
+        final String result = resource.get(String.class);
+
+        assertThat(result, containsString("com.datastax.driver.core.Cluster.basic-cluster"));
+    }
+
+    @Test
+    public void cassandraHealthCheckIsPublished() throws Exception {
+        final URI uri = UriBuilder.fromUri("http://localhost")
+                .port(APP.getLocalPort() + 1)
+                .path("healthcheck")
+                .build();
+
+        final WebResource resource = Client.create().resource(uri);
+        final String result = resource.get(String.class);
+
+        assertThat(result, containsString("cassandra.basic-cluster"));
+    }
+}
--- src/test/java/org.stuartgunter.dropwizard.cassandra/CassandraBundleFullIntegrationTest.java
+++ src/test/java/org.stuartgunter.dropwizard.cassandra/CassandraBundleFullIntegrationTest.java
@@ -16,7 +16,6 @@
 
 package org.stuartgunter.dropwizard.cassandra;
 
-import com.google.common.collect.Lists;
 import com.google.common.io.Resources;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
@@ -26,46 +25,18 @@ import org.junit.Test;
 
 import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
-import java.util.List;
 
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class CassandraBundleIntegrationTest {
+public class CassandraBundleFullIntegrationTest {
 
     @ClassRule
     public static final DropwizardAppRule<CassandraBundleConfiguration> APP =
-            new DropwizardAppRule<>(CassandraBundleApp.class, Resources.getResource("cassandra-bundle.yml").getPath());
+            new DropwizardAppRule<>(CassandraBundleApp.class, Resources.getResource("cassandra-full.yml").getPath());
 
     @Test
-    public void canQueryCassandra() throws Exception {
-        final URI uri = UriBuilder.fromUri("http://localhost")
-                .port(APP.getLocalPort())
-                .path("query")
-                .build();
-
-        final WebResource resource = Client.create().resource(uri);
-        final List<String> result = Lists.newArrayList(resource.get(String[].class));
-
-        assertThat(result, hasItem("system"));
-    }
-
-    @Test
-    public void cassandraMetricsArePublished() throws Exception {
-        final URI uri = UriBuilder.fromUri("http://localhost")
-                .port(APP.getLocalPort() + 1)
-                .path("metrics")
-                .build();
-
-        final WebResource resource = Client.create().resource(uri);
-        final String result = resource.get(String.class);
-
-        assertThat(result, containsString("com.datastax.driver.core.Cluster.my-cluster"));
-    }
-
-    @Test
-    public void cassandraHealthCheckIsPublished() throws Exception {
+    public void supportsFullConfiguration() throws Exception {
         final URI uri = UriBuilder.fromUri("http://localhost")
                 .port(APP.getLocalPort() + 1)
                 .path("healthcheck")
@@ -74,6 +45,6 @@ public class CassandraBundleIntegrationTest {
         final WebResource resource = Client.create().resource(uri);
         final String result = resource.get(String.class);
 
-        assertThat(result, containsString("cassandra.my-cluster"));
+        assertThat(result, containsString("cassandra.full-cluster"));
     }
 }
