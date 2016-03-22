cfb4f43662cb1e64c5352c945910ec2b1555ff8d
Stuart Gunter <stuart.gunter@gmail.com>
2016-03-22 07:03:23 +0000

    Added config properties for Netty SSL

--- src/main/java/org/stuartgunter/dropwizard/cassandra/ssl/NettySSLOptionsFactory.java
+++ src/main/java/org/stuartgunter/dropwizard/cassandra/ssl/NettySSLOptionsFactory.java
@@ -4,11 +4,14 @@ import com.datastax.driver.core.NettySSLOptions;
 import com.datastax.driver.core.SSLOptions;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
+import io.dropwizard.util.Duration;
+import io.netty.handler.ssl.ClientAuth;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.SslProvider;
 
 import javax.net.ssl.SSLException;
+import java.util.List;
 
 /**
  * A factory for configuring and building {@link NettySSLOptions} instances.
@@ -22,8 +25,28 @@ import javax.net.ssl.SSLException;
  *     </tr>
  *     <tr>
  *         <td>provider</td>
- *         <td>No default. Uses standard behavior from Netty.</td>
+ *         <td>No default.</td>
  *         <td>The {@link SslContext} implementation to use, as specified through the {@link SslProvider} enum.</td>
+ *     </tr>
+ *     <tr>
+ *         <td>ciphers</td>
+ *         <td>No default.</td>
+ *         <td>The cipher suites to enable, in the order of preference.</td>
+ *     </tr>
+ *     <tr>
+ *         <td>clientAuth</td>
+ *         <td>No default.</td>
+ *         <td>The client authentication mode.</td>
+ *     </tr>
+ *     <tr>
+ *         <td>sessionCacheSize</td>
+ *         <td>No default.</td>
+ *         <td>The size of the cache used for storing SSL session objects.</td>
+ *     </tr>
+ *     <tr>
+ *         <td>sessionTimeout</td>
+ *         <td>No default.</td>
+ *         <td>The timeout for the cached SSL session objects</td>
  *     </tr>
  * </table>
  */
@@ -31,6 +54,10 @@ import javax.net.ssl.SSLException;
 public class NettySSLOptionsFactory implements SSLOptionsFactory {
 
     private SslProvider provider;
+    private List<String> ciphers;
+    private ClientAuth clientAuth;
+    private Long sessionCacheSize;
+    private Duration sessionTimeout;
 
     @JsonProperty
     public SslProvider getProvider() {
@@ -42,12 +69,68 @@ public class NettySSLOptionsFactory implements SSLOptionsFactory {
         this.provider = provider;
     }
 
+    @JsonProperty
+    public List<String> getCiphers() {
+        return ciphers;
+    }
+
+    @JsonProperty
+    public void setCiphers(List<String> ciphers) {
+        this.ciphers = ciphers;
+    }
+
+    @JsonProperty
+    public ClientAuth getClientAuth() {
+        return clientAuth;
+    }
+
+    @JsonProperty
+    public void setClientAuth(ClientAuth clientAuth) {
+        this.clientAuth = clientAuth;
+    }
+
+    @JsonProperty
+    public Long getSessionCacheSize() {
+        return sessionCacheSize;
+    }
+
+    @JsonProperty
+    public void setSessionCacheSize(Long sessionCacheSize) {
+        this.sessionCacheSize = sessionCacheSize;
+    }
+
+    @JsonProperty
+    public Duration getSessionTimeout() {
+        return sessionTimeout;
+    }
+
+    @JsonProperty
+    public void setSessionTimeout(Duration sessionTimeout) {
+        this.sessionTimeout = sessionTimeout;
+    }
+
     @Override
     public SSLOptions build() {
         SslContextBuilder sslContextBuilder = SslContextBuilder.forClient();
 
         if (provider != null) {
             sslContextBuilder.sslProvider(provider);
+        }
+
+        if (clientAuth != null) {
+            sslContextBuilder.clientAuth(clientAuth);
+        }
+
+        if (sessionCacheSize != null) {
+            sslContextBuilder.sessionCacheSize(sessionCacheSize);
+        }
+
+        if (sessionTimeout != null) {
+            sslContextBuilder.sessionTimeout(sessionTimeout.toSeconds());
+        }
+
+        if (ciphers != null) {
+            sslContextBuilder.ciphers(ciphers);
         }
 
         SslContext sslContext;
