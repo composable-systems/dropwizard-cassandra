589179874b37b77517c52ab800ea9b85ea5640d6
Malte Pickhan <malte.pickhan@zalando.de>
2017-04-03 15:55:32 +0200

    Issue #25 Add javadoc

--- src/main/java/systems/composable/dropwizard/cassandra/connect/EC2MultiRegionAddressTranslatorFactory.java
+++ src/main/java/systems/composable/dropwizard/cassandra/connect/EC2MultiRegionAddressTranslatorFactory.java
@@ -4,6 +4,7 @@ import com.datastax.driver.core.policies.AddressTranslator;
 import com.datastax.driver.core.policies.EC2MultiRegionAddressTranslator;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
+/** A factory for configuring and building {@link com.datastax.driver.core.policies.EC2MultiRegionAddressTranslator} instances. */
 @JsonTypeName("EC2MultiRegionAddressTranslator")
 public class EC2MultiRegionAddressTranslatorFactory implements AddressTranslatorFactory {
   @Override
--- src/main/java/systems/composable/dropwizard/cassandra/connect/IdentityTranslatorFactory.java
+++ src/main/java/systems/composable/dropwizard/cassandra/connect/IdentityTranslatorFactory.java
@@ -4,6 +4,7 @@ import com.datastax.driver.core.policies.AddressTranslator;
 import com.datastax.driver.core.policies.IdentityTranslator;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
+/** A factory for configuring and building {@link com.datastax.driver.core.policies.IdentityTranslator} instances. */
 @JsonTypeName("IdentityTranslator")
 public class IdentityTranslatorFactory implements AddressTranslatorFactory {
   @Override
